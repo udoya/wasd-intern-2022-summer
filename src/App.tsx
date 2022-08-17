6f5d00c235460f05548be1e15a387980dbec5abf
Yamada <yammtch@keio.jp>
2022-08-17 13:13:36 +0900

    Componentに分割

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,70 @@
 import React from "react";
 import "bulma/css/bulma.css";
 
+const Header = () => {
+  return (
+    <header className="hero is-dark is-bold">
+      <div className="hero-body">
+        <div className="container"></div>
+        <h1 className="title">Cute Dog Images</h1>
+      </div>
+    </header>
+  );
+};
+
+const Image = () => {
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image">
+          <img src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg" alt="cute dog" />
+        </figure>
+      </div>
+    </div>
+  );
+};
+
+const Gallery = () => {
+  return (
+    <div className="columns is-vcentered is-multiline">
+      <div className="columns is-3">
+        <Image />
+      </div>
+    </div>
+  );
+};
+
+const Main = () => {
+  return (
+    <main>
+      <section className="section">
+        <div className="container">
+          <Gallery />
+        </div>
+      </section>
+    </main>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <p> Dog images are retrieved </p>
+        <p>
+          <a href="https://dog.ceo/dog-api/about"> Donate to Dog API </a>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="hero is-dark is-bold">
-        <div className="hero-body">
-          <div className="container"></div>
-          <h1 className="title">Cute Dog Images</h1>
-        </div>
-      </header>
-      <main>
-        <section className="section">
-          <div className="container">
-            <div className="columns is-vcentered is-multiline">
-              <div className="columns is-3">
-                <div className="card">
-                  <div className="card-image">
-                    <figure className="image">
-                      <img src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg" alt="cute dog" />
-                    </figure>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-      </main>
-      <footer className="footer">
-        <div className="content has-text-centered">
-          <p> Dog images are retrieved </p>
-          <p>
-            <a href="https://dog.ceo/dog-api/about"> Donate to Dog API </a>
-          </p>
-        </div>
-      </footer>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 };
