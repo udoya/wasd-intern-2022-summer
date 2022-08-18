58e6911bf056011656f27df967c03d3288253995
Yamada <yammtch@keio.jp>
2022-08-18 11:11:39 +0900

    imgタグ内のsrc,altを変数url,altTextを埋め込むように書き換え

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 
 export const Image: React.FC = () => {
+  const url: string = "https://images.dog.ceo/breeds/shiba/shiba-8.jpg";
+  const altText: string = "cure dog";
+
   return (
     <div className="card">
       <div className="card-image">
         <figure className="image">
-          <img src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg" alt="cute dog" />
+          <img src={url} alt={altText} />
         </figure>
       </div>
     </div>
