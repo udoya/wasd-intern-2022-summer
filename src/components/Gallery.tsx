327393dab3eb9ace15ecb254e0d94b1839c3e720
Yamada <yammtch@keio.jp>
2022-08-19 11:07:18 +0900

    GalleryにUrlsProを追加し、配列としてUrlを引き渡すように

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -2,14 +2,23 @@ import React from "react";
 
 import { Image } from "./Image";
 
-export const Gallery: React.FC = () => {
-  const url = "https://images.dog.ceo/breeds/shiba/shiba-8.jpg";
+type UrlsProps = {
+  urls: string[];
+};
+
+export const Gallery: React.FC<UrlsProps> = (props) => {
+  const { urls } = props;
   const altText = "cute dog";
+
   return (
     <div className="columns is-vcentered is-multiline">
-      <div className="columns is-3">
-        <Image url={url} altText={altText} />
-      </div>
+      {urls.map((url) => {
+        return (
+          <div key={url} className="column is-3">
+            <Image url={url} altText={altText} />
+          </div>
+        );
+      })}
     </div>
   );
 };
