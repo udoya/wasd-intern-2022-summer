1aa6992b37e792b669a943d0600c82ce173c6d7c
Yamada <yammtch@keio.jp>
2022-08-23 15:32:34 +0900

    fetch関数を一元化して書き直した

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,23 +1,21 @@
 import axios from "axios";
 
-export const fetchImages = async (breed: string) => {
+//引数にbreedを指定しない: breedsの配列を取得
+//引数にbreedを指定する:  そのbreedの画像URLをもった配列を取得
+export const fetchImages = async (breed?: string) => {
   try {
-    const response = await axios.get(`https://dog.ceo/api/breed/${breed}/images/random/12`);
-    const data = await response.data;
+    if (!breed) {
+      const response = await axios.get(`https://dog.ceo/api/breeds/list/all`);
+      const data = await response.data;
+      const breeds = Object.keys(data.message);
 
-    return data.message;
-  } catch (error) {
-    throw error;
-  }
-};
+      return breeds;
+    } else {
+      const response = await axios.get(`https://dog.ceo/api/breed/${breed}/images/random/12`);
+      const data = await response.data;
 
-export const fetchBreeds = async () => {
-  try {
-    const response = await axios.get(`https://dog.ceo/api/breeds/list/all`);
-    const data = await response.data;
-    const breedArray = Object.keys(data.message);
-
-    return breedArray;
+      return data.message;
+    }
   } catch (error) {
     throw error;
   }
